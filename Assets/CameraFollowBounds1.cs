b9a5c84de15909efcdd112bdcb34f665b4aee024
Nttlong <zugeliang2000@gmail.com>
2025-12-20 19:34:08 +0700

    fix

---
+++ Assets/CameraFollowBounds1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraFollowBounds1 : MonoBehaviour
+{
+    public Transform target;
+    public float minX;
+    public float maxX;
+
+    void Start()
+    {
+        Debug.Log("CameraFollowBounds STARTED");
+    }
+
+    void LateUpdate()
+    {
+        Debug.Log("CameraFollowBounds UPDATE RUNNING");
+
+        if (target == null)
+        {
+            Debug.LogWarning("CameraFollowBounds: TARGET = NULL");
+            return;
+        }
+
+        float newX = Mathf.Clamp(target.position.x, minX, maxX);
+        transform.position = new Vector3(
+            newX,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+}
